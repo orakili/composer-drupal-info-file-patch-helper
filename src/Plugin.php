26dc73c0ad6a55486c09c75fd1cd091c79f98967
orakili <github@orakili.net>
2022-12-20 03:47:48 +0000

    fix: compatibility with PHP 8.2
    Store the extra info in the plugin instead of adding a dynamic property to the package object which is deprecated in PHP 8.2

--- src/Plugin.php
+++ src/Plugin.php
@@ -32,6 +32,13 @@ final class Plugin implements PluginInterface, EventSubscriberInterface {
   private Composer $composer;
 
   /**
+   * Stored drupal module extra info (keyed by package unique name).
+   *
+   * @var array
+   */
+  protected array $drupalModuleExtraInfo = [];
+
+  /**
    * {@inheritdoc}
    */
   public function activate(Composer $composer, IOInterface $io): void {
@@ -77,20 +84,24 @@ final class Plugin implements PluginInterface, EventSubscriberInterface {
 
     $pattern = '@(\n+)(' . preg_quote(static::DRUPAL_INFO_DELIMITER) . '.+)@sm';
 
+    $drupal_module_extra_info = [];
     foreach (glob($install_path . '/*.info.yml') as $file) {
       $content = file_get_contents($file);
+      if ($content !== FALSE) {
+        // Extract, store and remove the extra info added by Drupal.
+        $changed_content = preg_replace_callback($pattern, function ($matches) use ($file, &$drupal_module_extra_info) {
+          $drupal_module_extra_info[$file] = $matches[2];
+          return "\n";
+        }, $content);
 
-      // Extract, store and remove the extra info added by Drupal.
-      $changed_content = preg_replace_callback($pattern, function ($matches) use ($file, $package) {
-        $package->drupalModuleExtraInfo = [
-          $file => $matches[2],
-        ];
-        return "\n";
-      }, $content);
-
-      if ($changed_content !== $content) {
-        file_put_contents($file, $changed_content);
+        if ($changed_content !== $content) {
+          file_put_contents($file, $changed_content);
+        }
       }
+    }
+
+    if (!empty($drupal_module_extra_info)) {
+      $this->drupalModuleExtraInfo[$package->getUniqueName()] = $drupal_module_extra_info;
     }
   }
 
@@ -107,14 +118,18 @@ final class Plugin implements PluginInterface, EventSubscriberInterface {
       return;
     }
 
-    if (isset($package->drupalModuleExtraInfo)) {
-      foreach ($package->drupalModuleExtraInfo as $file => $info) {
-        $content = file_get_contents($file);
-        if (!str_contains($content, $info)) {
-          $content = rtrim($content) . "\n\n" . $info;
-          file_put_contents($file, $content);
+    $unique_name = $package->getUniqueName();
+    if (isset($this->drupalModuleExtraInfo[$unique_name])) {
+      foreach ($this->drupalModuleExtraInfo[$unique_name] as $file => $info) {
+        if (file_exists($file)) {
+          $content = file_get_contents($file);
+          if ($content !== FALSE && !str_contains($content, $info)) {
+            $content = rtrim($content) . "\n\n" . $info;
+            file_put_contents($file, $content);
+          }
         }
       }
+      unset($this->drupalModuleExtraInfo[$unique_name]);
     }
   }
 
